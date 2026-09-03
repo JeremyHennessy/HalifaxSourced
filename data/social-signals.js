8ed90810390b287ee94be03a6b19513058c1f49e
Jeremy Hennessy <jeremy.hennessy@gmail.com>
2026-09-03 16:26:52 -0300

    Promote refreshed source and social data

--- data/social-signals.js
+++ data/social-signals.js
@@ -1,6 +1,6 @@
 window.HALIFAX_SOCIAL_SIGNALS = {
   "version": 3,
-  "generatedAt": "2026-09-01T15:25:53.069Z",
+  "generatedAt": "2026-09-03T18:59:48.769Z",
   "graphVersion": "v25.0",
   "lookbackDays": 60,
   "summaryChars": 420,
